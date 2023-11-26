e0abcca2051f6270657a6ea82a45ad55878f3fc8
Grzegorz Brzeczek <gbrzeczek@dtiq.com>
2023-11-26 11:31:07 +0100

    Added migration with auditing

---
+++ src/TravelPlanner.Api/Infrastructure/Persistence/Migrations/20231126102924_AddedAuditing.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelPlanner.Api.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedAuditing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Created",
+                table: "Trips",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedBy",
+                table: "Trips",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastModified",
+                table: "Trips",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LastModifiedBy",
+                table: "Trips",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Created",
+                table: "TripPlaces",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedBy",
+                table: "TripPlaces",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastModified",
+                table: "TripPlaces",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LastModifiedBy",
+                table: "TripPlaces",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Trips");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Trips");
+
+            migrationBuilder.DropColumn(
+                name: "LastModified",
+                table: "Trips");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                table: "Trips");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "TripPlaces");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "TripPlaces");
+
+            migrationBuilder.DropColumn(
+                name: "LastModified",
+                table: "TripPlaces");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                table: "TripPlaces");
+        }
+    }
+}
