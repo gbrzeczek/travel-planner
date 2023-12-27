e9ceff3a2271ddb88e977f72b17bf7e3b3388566
gbrzeczek <grzegorzbrzeczek1@protonmail.com>
2023-12-28 00:01:36 +0100

    Added placeholders

--- src/travel-planner-web/src/app/app.routes.ts
+++ src/travel-planner-web/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginPageComponent } from './auth/login-page/login-page.component';
+import { MyTripsComponent } from './trips/my-trips/my-trips.component';
+import { ContactComponent } from './common/contact/contact.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +12,13 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginPageComponent,
+  },
+  {
+    path: 'my-trips',
+    component: MyTripsComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
   },
 ];
--- src/travel-planner-web/src/app/common/navbar/navbar-item-list/navbar-item-list.component.ts
+++ src/travel-planner-web/src/app/common/navbar/navbar-item-list/navbar-item-list.component.ts
@@ -1,22 +1,23 @@
 import { Component } from '@angular/core';
 import { NavbarItemComponent } from '../navbar-item/navbar-item.component';
 import { NavbarItem } from '../navbar-item/navbar-item.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'tp-navbar-item-list',
   standalone: true,
   template: `
     @for (item of items; track $index) {
-    <tp-navbar-item [item]="item" />
+      <tp-navbar-item [item]="item" [routerLink]="item.path" />
     }
   `,
   styleUrl: './navbar-item-list.component.scss',
-  imports: [NavbarItemComponent],
+  imports: [NavbarItemComponent, RouterLink],
 })
 export class NavbarItemListComponent {
   items: NavbarItem[] = [
-    { label: 'Home', path: '/home', icon: 'bi bi-house-door' },
-    { label: 'Trips', path: '/about', icon: 'bi bi-suitcase-lg' },
+    { label: 'Home', path: '/', icon: 'bi bi-house-door' },
+    { label: 'Trips', path: '/my-trips', icon: 'bi bi-suitcase-lg' },
     { label: 'Contact', path: '/contact', icon: 'bi bi-telephone' },
   ];
 }
